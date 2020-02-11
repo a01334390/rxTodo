db1c2e134f88cdb74b2e2920ad6884c2fde5cdb2
Fernando Martin Garcia Del Angel <martingarciadelangel@me.com>
2020-02-11 14:54:52 -0600

    Finished Task list View

---
+++ rxTodo/rxTodo/Controllers/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  rxTodo
+//
+//  Created by Fernando Martin Garcia Del Angel on 11/02/20.
+//  Copyright © 2020 Fernando Martin Garcia Del Angel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var prioritySegmentedControl: UISegmentedControl!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    // MARK - Table View Controllers
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TaskTableViewCell",for: indexPath)
+        return  cell
+    }
+
+}
+
